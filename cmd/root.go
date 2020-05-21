ca3e7281df2e3e12b9784ba2b38efdf85f944b4e
Humaun Kabir <psychopath.humaun@gmail.com>
2020-05-21 17:35:18 +0600

    Add cobra cli commands

---
+++ cmd/root.go
@@ -0,0 +1,63 @@
+package cmd
+
+import (
+	"fmt"
+
+	homedir "github.com/mitchellh/go-homedir"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var (
+	// Used for flags.
+	cfgFile     string
+	userLicense string
+
+	rootCmd = &cobra.Command{
+		Use:   "bookx",
+		Short: "An api for book informations",
+		Long: `A long desccription`,
+	}
+)
+
+// Execute executes the root command.
+func Execute() error {
+	return rootCmd.Execute()
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.bookx.yaml)")
+	rootCmd.PersistentFlags().StringP("author", "a", "HUMAUN KABIR", "author name for copyright attribution")
+	rootCmd.PersistentFlags().StringVarP(&userLicense, "license", "l", "", "name of license for the project")
+	rootCmd.PersistentFlags().Bool("viper", true, "use Viper for configuration")
+	viper.BindPFlag("author", rootCmd.PersistentFlags().Lookup("author"))
+	viper.BindPFlag("useViper", rootCmd.PersistentFlags().Lookup("viper"))
+	viper.SetDefault("author", "HUMAUN KABIR <ask.humaunkabir@gmail.com>")
+	viper.SetDefault("license", "MIT")
+
+}
+
+func initConfig() {
+	if cfgFile != "" {
+		// Use config file from the flag.
+		viper.SetConfigFile(cfgFile)
+	} else {
+		// Find home directory.
+		home, err := homedir.Dir()
+		if err != nil {
+			fmt.Println("Error Occured, root.go, homedir")
+		}
+
+		// Search config in home directory with name ".bookx" (without extension).
+		viper.AddConfigPath(home)
+		viper.SetConfigName(".bookx")
+	}
+
+	viper.AutomaticEnv()
+
+	if err := viper.ReadInConfig(); err == nil {
+		fmt.Println("Using config file:", viper.ConfigFileUsed())
+	}
+}
