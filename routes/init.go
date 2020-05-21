c16674c1e9b123c0f70a54638998098d05982d95
Humaun Kabir <psychopath.humaun@gmail.com>
2020-05-21 17:34:53 +0600

    Create book api routes

---
+++ routes/init.go
@@ -0,0 +1,31 @@
+package routes
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	books "github.com/oasiscse/bookx/api/books"
+
+)
+
+func GetRoutes() {
+
+	r := mux.NewRouter()
+
+  	r.HandleFunc("/", homepage).Methods("GET")
+	r.HandleFunc("/api/books/info/all", books.GetBooks).Methods("GET")
+	r.HandleFunc("/api/books/info/{bookid}", books.GetByID).Methods("GET")
+	r.HandleFunc("/api/books/remove/{bookid}", books.DeleteByID).Methods("DELETE")
+	r.HandleFunc("/api/books/info/all/{year}", books.GetByYear).Methods("GET")
+	r.HandleFunc("/api/books/{title}/{author}/{psher}/{year}/{cat}/{bookid}", books.Create).Methods("POST")
+
+	log.Fatal(http.ListenAndServe(":3000", r))
+}
+
+
+func homepage(w http.ResponseWriter, r *http.Request){
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write([]byte(`{"bookx" : "A clever monkey is cooking for you, wait..."}`))
+}
