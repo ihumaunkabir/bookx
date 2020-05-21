cb384221b927fc2eb638d5412222232ed5b4f354
Humaun Kabir <psychopath.humaun@gmail.com>
2020-05-21 17:34:02 +0600

    Create connection with mongodb

---
+++ conn/conn.go
@@ -0,0 +1,52 @@
+package conn
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+
+func ConnectDB() *mongo.Collection {
+
+	
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+	//clientOptions := options.Client().ApplyURI("mongodb://bookx:bookx123@ds217809.mlab.com:17809/bookx?retryWrites=false")
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Connected to MongoDB!")
+
+	collection := client.Database("bookx").Collection("books")
+
+	return collection
+}
+
+
+type ErrorResponse struct {
+	StatusCode   int    `json:"status"`
+	ErrorMessage string `json:"message"`
+}
+
+
+func GetError(err error, w http.ResponseWriter) {
+
+	log.Fatal(err.Error())
+	var response = ErrorResponse{
+		ErrorMessage: err.Error(),
+		StatusCode:   http.StatusInternalServerError,
+	}
+
+	message, _ := json.Marshal(response)
+
+	w.WriteHeader(response.StatusCode)
+	w.Write(message)
+}
