fcf2667c430d73cb0d1667cadbbb9ee11b8192a0
Humaun Kabir <psychopath.humaun@gmail.com>
2020-06-17 17:58:25 +0600

    Update create request

--- api/books/create.go
+++ api/books/create.go
@@ -1,43 +1,26 @@
 package books
 
-import(
+import (
 	"context"
 	"encoding/json"
 	"net/http"
 
-	"github.com/gorilla/mux"
 	"github.com/oasiscse/bookx/conn"
-	"github.com/oasiscse/bookx/data/api"
+	data "github.com/oasiscse/bookx/data/api"
 )
 
-func Create(w http.ResponseWriter, r *http.Request){
+func Create(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Content-Type", "application/json")
-	
-	pms := mux.Vars(r)
 
-	author := pms["author"]
-	title := pms["title"]
-	psher := pms["psher"]
-	year := pms["year"]
-	cat := pms["cat"]
-	bookid := pms["bookid"]
-
-	book := data.Book{
-		Title : title,
-		Publisher : psher,
-		Author : author,
-		Year : year,
-		Category : cat,
-		BookID : bookid,
-	}	
-
+	var book data.Book
+	_ = json.NewDecoder(r.Body).Decode(&book)
 	collection := conn.ConnectDB()
 	result, err := collection.InsertOne(context.TODO(), book)
 
 	if err != nil {
-		w.Write([]byte(`{"message" : "Can not crate book"}`))
-	} else{
+		w.Write([]byte(`{"message" : "Can not Create book"}`))
+	} else {
 		json.NewEncoder(w).Encode(result)
 	}
 
