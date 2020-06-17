9394274d0be8e4f6cc2a6ef97cfa778a17c53a51
Humaun Kabir <psychopath.humaun@gmail.com>
2020-06-17 17:57:40 +0600

    Update create route

--- routes/init.go
+++ routes/init.go
@@ -6,25 +6,23 @@ import (
 
 	"github.com/gorilla/mux"
 	books "github.com/oasiscse/bookx/api/books"
-
 )
 
 func GetRoutes() {
 
 	r := mux.NewRouter()
 
-  	r.HandleFunc("/", homepage).Methods("GET")
+	r.HandleFunc("/", homepage).Methods("GET")
 	r.HandleFunc("/api/books/info/all", books.GetBooks).Methods("GET")
 	r.HandleFunc("/api/books/info/{bookid}", books.GetByID).Methods("GET")
 	r.HandleFunc("/api/books/remove/{bookid}", books.DeleteByID).Methods("DELETE")
 	r.HandleFunc("/api/books/info/all/{year}", books.GetByYear).Methods("GET")
-	r.HandleFunc("/api/books/{title}/{author}/{psher}/{year}/{cat}/{bookid}", books.Create).Methods("POST")
+	r.HandleFunc("/api/books/create", books.Create).Methods("POST")
 
 	log.Fatal(http.ListenAndServe(":3000", r))
 }
 
-
-func homepage(w http.ResponseWriter, r *http.Request){
+func homepage(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Content-Type", "application/json")
 	w.Write([]byte(`{"bookx" : "A clever monkey is cooking for you, wait..."}`))
