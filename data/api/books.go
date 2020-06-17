a68e6577610889f4b297464f9429f97ef824d81a
Humaun Kabir <psychopath.humaun@gmail.com>
2020-06-17 17:58:03 +0600

    Fix struct format

--- data/api/books.go
+++ data/api/books.go
@@ -2,13 +2,12 @@ package data
 
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
-
 type Book struct {
-	ID     primitive.ObjectID 	`json:"_id,omitempty" bson:"_id,omitempty"`
-    Title  string       `json:"title,omitempty" bson:"title,omitempty"`
-	Author string       `json:"author, omitempty" bson:"author,omitempty"`
-	Publisher   string  `json:"psher,omitempty" bson:"psher,omitempty"`
-	Year string 		`json:"year,omitempty" bson:"year,omitempty"`
-	Category string 	`json:"cat, omitempty" bson:"cat, omitempty"`
-	BookID string 		`json:"bookid, omitempty" bson:"bookid, omitempty"`
+	ID        primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	Title     string             `json:"title,omitempty" bson:"title,omitempty"`
+	Author    string             `json:"author,omitempty" bson:"author,omitempty"`
+	Publisher string             `json:"psher,omitempty" bson:"psher,omitempty"`
+	Year      string             `json:"year,omitempty" bson:"year,omitempty"`
+	Category  string             `json:"cat,omitempty" bson:"cat,omitempty"`
+	BookID    string             `json:"bookid,omitempty" bson:"bookid,omitempty"`
 }
