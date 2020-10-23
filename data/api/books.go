0fb62659269e5a0304e1b62e1b1ada9ce25b3eff
Humaun Kabir <psychopath.humaun@gmail.com>
2020-10-23 20:28:54 +0600

    Refactor api data

--- data/api/books.go
+++ data/api/books.go
@@ -1,18 +1,20 @@
 package data
 
 import (
-	data "bookx/data/api"
+	"time"
 
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
+//Book holds struct for book information
 type Book struct {
 	ID        primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
-	Basic     data.Basic         `json:",inline"`
 	Title     string             `json:"title,omitempty" bson:"title,omitempty"`
 	Author    string             `json:"author,omitempty" bson:"author,omitempty"`
 	Publisher string             `json:"psher,omitempty" bson:"psher,omitempty"`
 	Year      string             `json:"year,omitempty" bson:"year,omitempty"`
 	Category  string             `json:"cat,omitempty" bson:"cat,omitempty"`
 	BookID    string             `json:"bookid,omitempty" bson:"bookid,omitempty"`
+	CreatedAt time.Time          `json:"created_at,omitempty" bson:"created_at,omitempty"`
+	UpdatedAt time.Time          `json:"updated_at,omitempty" bson:"updated_at,omitempty"`
 }
